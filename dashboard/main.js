9af194141e9fdad6a935cf6c7749ebf52177bcbb
earthperson <ponomarev.dev@gmail.com>
2014-04-30 16:24:49 +0400

    Fixed commit c22fc447398a3f054462ba63ba17b46df0c61463

--- dashboard/main.js
+++ dashboard/main.js
@@ -2,13 +2,14 @@ $(function() {
 	$.fn.luhn = function() {
 		var number = [], $this = this;
 		$('input', this).keyup(function() {
-			number[$(this).index()] = parseInt($.trim($(this).val()));
+			number[$(this).index()] = $.trim($(this).val());
 			if(number.length == 4 && luhn(number.join(''))) {
 				$this.removeClass('has-error').addClass('has-success');
 			}
 			else {
 				$this.removeClass('has-success').addClass('has-error');
 			}
+			console.log(number.join(''));
 		});
 	};
 	$('#luhn').luhn();
