f510f88321d004c84f1e519b333fb9349cb44997
earthperson <ponomarev.dev@gmail.com>
2014-05-28 22:25:35 +0400

    Starting data rate

---
+++ datarate/datarate.js
@@ -0,0 +1,19 @@
+(function() {
+	
+	var toSeconds = function() {
+
+	};
+	
+	var toBytes = function() {
+
+	};
+	
+	var datarate = {};
+	
+	datarate.getDownloadTime = function() {
+
+	};
+	
+	window.datarate = datarate;
+	
+})(window);
